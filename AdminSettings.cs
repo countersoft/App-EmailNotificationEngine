72de7c48bdf94f74e07208a4c545ede66007cbfd
OblivionSY <syates@interaction-online.com>
2017-10-10 15:02:13 +0100

    Add debug info for Workspace

---
+++ AdminSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Countersoft.Gemini.Infrastructure.Apps;
+
+namespace EmailNotificationEngine
+{
+    
+}
